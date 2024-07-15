f06295d8fcee457d9f480a30982b3651127c56fa
CedricNtwari <cedric.ntwari@powercoders.org>
2024-07-15 16:44:05 +0200

    feat(navbar): Add and style navbar with React-Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css'
-import Button from 'react-bootstrap/Button'
+import styles from '../src/css/App.module.css'
+import NavBar from '../src/components/NavBar'
 
 function App() {
   return (
-    <div className="App">
-      <h1>React App</h1>
-      <Button variant="primary">Primary</Button>
+    <div className={styles.App}>
+      <NavBar />
     </div>
   )
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Navbar, Container, Nav } from 'react-bootstrap'
+import logo from '../assets/logo.png'
+
+const NavBar = () => {
+  return (
+    <Navbar expand="md" fixed="top">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={logo} alt="logo" width="50" height="45" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <Nav.Link href="#">
+              <i className="fas fa-home"></i>Home
+            </Nav.Link>
+            <Nav.Link href="#">
+              <i className="fas fa-sign-in-alt"></i>Sign In
+            </Nav.Link>
+            <Nav.Link href="#">
+              <i className="fas fa-user-plus"></i>Sign Out
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default NavBar
