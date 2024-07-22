578f7e38576b6dfc534e5e0f2bf8031a25574f31
CedricNtwari <cedric.ntwari@powercoders.org>
2024-07-22 20:10:47 +0200

    Added Dynamic Post Fetching and Display to PostsPage Component

--- src/App.js
+++ src/App.js
@@ -6,19 +6,44 @@ import './api/axiosDefaults'
 import SignUpForm from './pages/auth/SignUpForm'
 import SignInForm from './pages/auth/ SignInForm'
 import PostCreateForm from './pages/posts/PostCreateForm'
+import PostsPage from './pages/posts/PostsPage'
 import PostPage from './pages/posts/PostPage'
+import { useCurrentUser } from './contexts/CurrentUserContext'
 
 function App() {
+  const currentUser = useCurrentUser()
+  const profile_id = currentUser?.profile_id || ''
+
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route
+            exact
+            path="/"
+            render={() => <PostsPage message="No results found. Adjust the search keyword" />}
+          />
           <Route path="/signin" render={() => <SignInForm />} />
           <Route path="/signup" render={() => <SignUpForm />} />
-          <Route path="/feed" render={() => <h1>Feed page</h1>} />
-          <Route path="/liked" render={() => <h1>Liked page</h1>} />
+          <Route
+            path="/feed"
+            render={() => (
+              <PostsPage
+                message="No results found. Adjust the search keyword or felllow a user"
+                filter={`owner__followed__owner__profile=${profile_id}&`}
+              />
+            )}
+          />
+          <Route
+            path="/liked"
+            render={() => (
+              <PostsPage
+                message="No results found. Adjust the search keyword or felllow a user"
+                filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
+              />
+            )}
+          />
           <Route path="/posts/create" render={() => <PostCreateForm />} />
           <Route path="/posts/:id" render={() => <PostPage />} />
           <Route render={() => <h1>Page not found!!!</h1>} />
