c9fa904d687d7ce619e344201ea51037720c61f8
CedricNtwari <cedric.ntwari@powercoders.org>
2024-07-23 17:12:58 +0200

    Add PopularProfiles Component and Integrate with PostsPage

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -16,6 +16,7 @@ import { axiosReq } from '../../api/axiosDefaults'
 import NoResults from '../../assets/no-results.png'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import { fetchMoreData } from '../../utils/utils'
+import PopularProfiles from '../profiles/PopularProfiles'
 
 function PostsPage({ message, filter = '' }) {
   const [posts, setPosts] = useState({ results: [] })
@@ -34,15 +35,21 @@ function PostsPage({ message, filter = '' }) {
         console.log(err)
       }
     }
+
     setHasLoaded(false)
-    const timer = setTimeout(() => fetchPosts(), 1000)
-    return () => clearTimeout(timer)
+    const timer = setTimeout(() => {
+      fetchPosts()
+    }, 1000)
+
+    return () => {
+      clearTimeout(timer)
+    }
   }, [filter, query, pathname])
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Popular profiles mobile</p>
+        <PopularProfiles mobile />
         <i className={`fas fa-search ${styles.SearchIcon}`} />
         <Form className={styles.SearchBar} onSubmit={(event) => event.preventDefault()}>
           <Form.Control
@@ -53,6 +60,7 @@ function PostsPage({ message, filter = '' }) {
             placeholder="Search posts"
           />
         </Form>
+
         {hasLoaded ? (
           <>
             {posts.results.length ? (
@@ -78,7 +86,7 @@ function PostsPage({ message, filter = '' }) {
         )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        <p>Popular profiles for desktop</p>
+        <PopularProfiles />
       </Col>
     </Row>
   )
