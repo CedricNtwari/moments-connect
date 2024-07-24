4e2b5228269866b10410c06077be25e2c000483e
CedricNtwari <cedric.ntwari@powercoders.org>
2024-07-24 20:38:18 +0200

    Added new backend API url

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,6 +1,6 @@
 import { rest } from 'msw'
 
-const baseURL = 'https://drf-api-rec.herokuapp.com/'
+const baseURL = 'https://drf-api-projet-0f572d5cbd12.herokuapp.com/'
 
 export const handlers = [
   rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
